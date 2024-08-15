5f965986a77d45e379aa5632bf7752672a70a389
James0711 <jengreat94@gmail.com>
2024-08-15 07:54:51 +0100

    added lines of code

--- app/database.py
+++ app/database.py
@@ -4,20 +4,27 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Load environment variables from .env file
 load_dotenv()
 
-
+# Load environment variables from .env file
 SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
 
+# Create SQLAlchemy engine
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
+
+
+# Create a session maker bound to the engine
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Base class for declarative models
 Base = declarative_base()
 
 
 def get_db():
+     # Provide a database session to be used in dependency injection
     db = SessionLocal()
     try:
         yield db
