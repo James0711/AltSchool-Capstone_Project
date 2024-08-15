10755c94061e6652cb6fa46dcfb8be85e442087a
James0711 <jengreat94@gmail.com>
2024-08-15 08:00:49 +0100

    added an endpoint

--- app/routers/users.py
+++ app/routers/users.py
@@ -10,7 +10,7 @@ from app.database import get_db
 
 user_router = APIRouter()
 
-
+# Endpoint to get a list of users
 @user_router.get("/", status_code=200, response_model=List[schemas.User])
 async def get_users(db: Session = Depends(get_db), offset: int = 0, limit: int = 10):
     users = user_crud_service.get_users(
@@ -20,7 +20,7 @@ async def get_users(db: Session = Depends(get_db), offset: int = 0, limit: int =
     )
     return users
 
-
+# Endpoint to get a single user by ID
 @user_router.get("/{user_id}", status_code=200, response_model=schemas.User)
 async def get_user_by_id(user_id: str, db: Session = Depends(get_db)):
     user = user_crud_service.get_user_by_id(db, user_id)
@@ -29,7 +29,7 @@ async def get_user_by_id(user_id: str, db: Session = Depends(get_db)):
             status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return user
 
-
+# Endpoint to get a single user by username
 @user_router.get("/name/{username}", status_code=200, response_model=schemas.User)
 async def get_user_by_username(username: str, db: Session = Depends(get_db)):
     user = user_crud_service.get_user_by_username(db, username)
@@ -38,7 +38,7 @@ async def get_user_by_username(username: str, db: Session = Depends(get_db)):
                             detail="User not found")
     return user
 
-
+# Endpoint to update a user by ID
 @user_router.put("/{user_id}", status_code=200, response_model=schemas.User)
 async def update_user(user_id: int, payload: schemas.UserUpdate, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
     db_user = user_crud_service.get_user_by_id(db, user_id=user_id)
@@ -53,7 +53,7 @@ async def update_user(user_id: int, payload: schemas.UserUpdate, db: Session = D
 
     return user
 
-
+# Endpoint to delete a user by ID
 @user_router.delete("/{user_id}", status_code=200)
 async def delete_user(user_id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
     user = user_crud_service.get_user_by_id(db, user_id=user_id)
