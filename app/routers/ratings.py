5a80a40a10fbd3930d34621bf0200b4391196eef
James0711 <jengreat94@gmail.com>
2024-08-16 18:41:21 +0100

    added some lines of code

--- app/routers/ratings.py
+++ app/routers/ratings.py
@@ -1,47 +1,32 @@
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status
-from app.auth import get_current_user
-from app.logger import logger
-import app.schemas as schemas
-from app.crud import rating_crud_service, movie_crud_service
 from sqlalchemy.orm import Session
 import app.schemas as schemas
+from app.crud import rating_crud_service, movie_crud_service
 from app.database import get_db
+from app.auth import get_current_user
+from app.logger import logger
 
 rating_router = APIRouter()
 
 @rating_router.get("/", status_code=200, response_model=List[schemas.Rating])
 async def get_ratings(db: Session = Depends(get_db), offset: int = 0, limit: int = 10):
-    ratings = rating_crud_service.get_ratings(
-        db,
-        offset=offset,
-        limit=limit
-    )
+    ratings = rating_crud_service.get_ratings(db, offset=offset, limit=limit)
     return ratings
-
 
 @rating_router.get("/{rating_id}", status_code=200, response_model=schemas.Rating)
 async def get_rating_by_id(rating_id: int, db: Session = Depends(get_db)):
-    rating = rating_crud_service.get_rating_by_id(
-        db,
-        rating_id=rating_id
-    )
+    rating = rating_crud_service.get_rating_by_id(db, rating_id)
     if not rating:
-        raise HTTPException(status_code=404, detail="Rating not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Rating not found")
     return rating
 
-
-@rating_router.get("/movie_id/{movie_id}", status_code=200, response_model=List[schemas.Rating])
+@rating_router.get("/movie/{movie_id}", status_code=200, response_model=List[schemas.Rating])
 async def get_ratings_by_movie_id(movie_id: int, db: Session = Depends(get_db), offset: int = 0, limit: int = 10):
     movie = movie_crud_service.get_movie_by_id(db, movie_id)
     if not movie:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Movie not found")
-    ratings = rating_crud_service.get_ratings_by_movie_id(
-        db,
-        movie_id=movie_id,
-        offset=offset,
-        limit=limit
-    )
+    ratings = rating_crud_service.get_ratings_by_movie_id(db, movie_id, offset, limit)
     return ratings
 
 @rating_router.get("/average_rating/{movie_id}", status_code=200)
@@ -56,57 +41,40 @@ async def get_movie_avg_rating(movie_id: int, db: Session = Depends(get_db)):
         "owner_id": movie.user_id,
         "avg_rating": avg_rating
     }
-
-    return {"message": "successful", "data": data}
-
+    return {"message": "Successful", "data": data}
 
 @rating_router.post('/{movie_id}', status_code=201, response_model=schemas.Rating)
 async def rate_movie(movie_id: int, rating: schemas.RatingCreate, current_user: schemas.User = Depends(get_current_user), db: Session = Depends(get_db)):
     movie = movie_crud_service.get_movie_by_id(db, movie_id)
     if not movie:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Movie not found")
-    # Query to see if there is an existing rating
-
-    db_rating = rating_crud_service.get_rating(db, user_id=current_user.id, movie_id=movie_id)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Movie not found")
     
-    # Check if user has already rated movie, if true then update existing ratings
-    if db_rating is not None:
-        logger.warning("User trying to rate an already rated movie...")
-        raise HTTPException(status.HTTP_409_CONFLICT, detail="You have already rated movie. Update existing rating")
+    existing_rating = rating_crud_service.get_rating(db, user_id=current_user.id, movie_id=movie_id)
+    if existing_rating is not None:
+        logger.warning(f"User {current_user.id} is trying to rate movie {movie_id} again.")
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="You have already rated this movie. Update your existing rating instead.")
 
-    new_rating = rating_crud_service.rate_movie_by_id(
-        db,
-        rating=rating,
-        user_id=current_user.id,
-        movie_id=movie_id
-    )
+    new_rating = rating_crud_service.rate_movie_by_id(db, rating=rating, user_id=current_user.id, movie_id=movie_id)
     return new_rating
-
 
 @rating_router.put("/{rating_id}", status_code=200, response_model=schemas.Rating)
 async def update_rating(rating_id: int, payload: schemas.RatingUpdate, current_user: schemas.User = Depends(get_current_user), db: Session = Depends(get_db)):
     rating = rating_crud_service.get_rating_by_id(db, rating_id)
     if not rating:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Rating not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Rating not found")
     if rating.user_id != current_user.id:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authorized")
-   
-    update_rating = rating_crud_service.update_rating(db,rating_payload=payload, rating_id=rating_id)
-
-    return update_rating
-
+    
+    updated_rating = rating_crud_service.update_rating(db, rating_payload=payload, rating_id=rating_id)
+    return updated_rating
 
 @rating_router.delete("/{rating_id}", status_code=200)
 async def delete_rating(rating_id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
-    rating = rating_crud_service.get_rating_by_id(db, rating_id=rating_id)
+    rating = rating_crud_service.get_rating_by_id(db, rating_id)
     if not rating:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Rating not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Rating not found")
     if rating.user_id != current_user.id:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authorized")
-
-
-    rating_crud_service.delete_rating(db, rating_id=rating_id)
-    return {"message": "Successful"}
+    
+    rating_crud_service.delete_rating(db, rating_id)
+    return {"message": "Rating deleted successfully"}
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,32 +1,26 @@
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status
-from app.auth import get_current_user
-from app.logger import logger
-import app.schemas as schemas
-from app.crud import user_crud_service
 from sqlalchemy.orm import Session
 import app.schemas as schemas
+from app.crud import user_crud_service
 from app.database import get_db
+from app.auth import get_current_user
+from app.logger import logger
 
 user_router = APIRouter()
 
 # Endpoint to get a list of users
 @user_router.get("/", status_code=200, response_model=List[schemas.User])
 async def get_users(db: Session = Depends(get_db), offset: int = 0, limit: int = 10):
-    users = user_crud_service.get_users(
-        db,
-        offset=offset,
-        limit=limit
-    )
+    users = user_crud_service.get_users(db, offset=offset, limit=limit)
     return users
 
 # Endpoint to get a single user by ID
 @user_router.get("/{user_id}", status_code=200, response_model=schemas.User)
-async def get_user_by_id(user_id: str, db: Session = Depends(get_db)):
+async def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
     user = user_crud_service.get_user_by_id(db, user_id)
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return user
 
 # Endpoint to get a single user by username
@@ -34,35 +28,31 @@ async def get_user_by_id(user_id: str, db: Session = Depends(get_db)):
 async def get_user_by_username(username: str, db: Session = Depends(get_db)):
     user = user_crud_service.get_user_by_username(db, username)
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="User not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return user
 
 # Endpoint to update a user by ID
 @user_router.put("/{user_id}", status_code=200, response_model=schemas.User)
 async def update_user(user_id: int, payload: schemas.UserUpdate, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
-    db_user = user_crud_service.get_user_by_id(db, user_id=user_id)
+    db_user = user_crud_service.get_user_by_id(db, user_id)
     if not db_user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     if db_user.id != current_user.id:
-        logger.warning("User not authorized....")
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authorized")
+        logger.warning(f"User {current_user.id} attempted to update another user {user_id}.")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authorized")
+    
     user = user_crud_service.update_user(db, user_id, payload)
-
     return user
 
 # Endpoint to delete a user by ID
 @user_router.delete("/{user_id}", status_code=200)
 async def delete_user(user_id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
-    user = user_crud_service.get_user_by_id(db, user_id=user_id)
+    user = user_crud_service.get_user_by_id(db, user_id)
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     if user.id != current_user.id:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authorized")
-    user_crud_service.delete_user(db, user_id=user_id)
-
-    return {"message": "Successful"}
+        logger.warning(f"User {current_user.id} attempted to delete another user {user_id}.")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authorized")
+    
+    user_crud_service.delete_user(db, user_id)
+    return {"message": "User deleted successfully"}
